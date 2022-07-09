d8183590cd5940dbb35d35ffaeab342ecb6104e2
GIANJAJAHAHAHA <104642265+GIANJAJAHAHAHA@users.noreply.github.com>
2022-07-09 10:08:12 +0800

    Add files via upload

---
+++ src/module/montage/Choke.js
@@ -0,0 +1,28 @@
+const jimp = require(`jimp`);
+
+module.exports = class Choke {
+    /**
+     * Choke
+     * @param {image} image1 
+     * @param {image} image2 
+     */
+    async getImage(image1, image2) {
+        if (!image1) throw new Error(`You must provide an image as a first argument.`);
+        if (!image2) throw new Error(`You must provide an image as a second argument.`);
+        let base = await jimp.read(`modules/commands/templates/choke.png`);
+        image1 = await jimp.read(image1);
+        image2 = await jimp.read(image2);
+        image1.circle();
+        image2.circle();
+        base.resize(740, 415);
+        image1.resize(180, 180);
+        image2.resize(180, 180);
+        base.composite(image1, 150, 15);
+        base.composite(image2, 410, 60);
+        let raw;
+        base.getBuffer(`image/png`, (err, buffer) => {
+            raw = buffer;
+        });
+        return raw;
+    }
+};
---
+++ src/module/montage/arrest.js
@@ -0,0 +1,28 @@
+const jimp = require(`jimp`);
+
+module.exports = class Arrest {
+    /**
+     * Arrest
+     * @param {image} image1 
+     * @param {image} image2 
+     */
+    async getImage(image1, image2) {
+        if (!image1) throw new Error(`You must provide an image as a first argument.`);
+        if (!image2) throw new Error(`You must provide an image as a second argument.`);
+        let base = await jimp.read(`modules/commands/templates/arrest.png`);
+        image1 = await jimp.read(image1);
+        image2 = await jimp.read(image2);
+        image1.circle();
+        image2.circle();
+        base.resize(425, 722);
+        image1.resize(170, 170);
+        image2.resize(170, 170);
+        base.composite(image1, 50, 35);
+        base.composite(image2, 230, 120);
+        let raw;
+        base.getBuffer(`image/png`, (err, buffer) => {
+            raw = buffer;
+        });
+        return raw;
+    }
+};
