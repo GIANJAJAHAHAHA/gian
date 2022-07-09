271e29c5d4a09c2a1b66efe40098c678e8cdf87c
GIANJAJAHAHAHA <104642265+GIANJAJAHAHAHA@users.noreply.github.com>
2022-07-09 10:12:02 +0800

    Update arrest.js

--- src/module/montage/arrest.js
+++ src/module/montage/arrest.js
@@ -9,7 +9,7 @@ module.exports = class Arrest {
     async getImage(image1, image2) {
         if (!image1) throw new Error(`You must provide an image as a first argument.`);
         if (!image2) throw new Error(`You must provide an image as a second argument.`);
-        let base = await jimp.read(`modules/commands/templates/arrest.png`);
+        let base = await jimp.read(`${__dirname}/../../assets/arrest.png`);
         image1 = await jimp.read(image1);
         image2 = await jimp.read(image2);
         image1.circle();
